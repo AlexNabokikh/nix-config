5dd016f5c03da5a468f3c7a02c8f85dc9f7ad35d
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2023-12-26 13:03:08 +0100

    feat: added fstrim via nixos hardware
    feat: added energy

--- hosts/energy/configuration.nix
+++ hosts/energy/configuration.nix
@@ -9,7 +9,7 @@
   imports = [
     inputs.hardware.nixosModules.common-cpu-amd
     inputs.hardware.nixosModules.common-gpu-amd
-    inputs.hardware.nixosModules.common-ssd
+    inputs.hardware.nixosModules.common-pc-ssd
 
     ./hardware-configuration.nix
   ];
