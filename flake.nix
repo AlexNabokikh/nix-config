a74b65561f1657f6799d9ad680e4531498155607
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2023-12-26 09:49:34 +0100

    feat: added energy

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "Your new nix config";
+
+  inputs = {
+    # Nixpkgs
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+
+    # Home manager
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    # NixOS profiles to optimize settings for different hardware
+    hardware.url = "github:nixos/nixos-hardware";
+
+    # NixOS theming
+    nix-colors.url = "github:misterio77/nix-colors";
+
+    # NixOS Spicetify
+    spicetify-nix.url = github:the-argus/spicetify-nix;
+    spicetify-nix.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    ...
+  } @ inputs: let
+    inherit (self) outputs;
+  in {
+    nixosConfigurations = {
+      energy = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs outputs;};
+        modules = [./nixos/configuration.nix];
+      };
+    };
+
+    homeConfigurations = {
+      "nabokikh@energy" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        extraSpecialArgs = {inherit inputs outputs;};
+        modules = [./home-manager/home.nix];
+      };
+    };
+  };
+}
