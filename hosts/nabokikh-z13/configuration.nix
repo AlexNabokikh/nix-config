eafec1489a234a344e4787540de7f77d89c55f9d
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2023-12-27 09:13:47 +0100

    feat(z13): switched to the latest kernel
    feat: added energy

--- hosts/nabokikh-z13/configuration.nix
+++ hosts/nabokikh-z13/configuration.nix
@@ -41,7 +41,7 @@
   };
 
   # Bootloader.
-  # boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot = {
