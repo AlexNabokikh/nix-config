0437bb533709ad447e929538ba1efd793b8c14e4
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2023-12-27 08:07:53 +0100

    feat(energy): switched to the latest kernel
    feat: added energy

--- hosts/energy/configuration.nix
+++ hosts/energy/configuration.nix
@@ -43,7 +43,7 @@
   };
 
   # Bootloader.
-  # boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot = {
