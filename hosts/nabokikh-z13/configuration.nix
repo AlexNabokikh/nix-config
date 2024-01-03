c81235350081919b11de4c851d60e449c5f0acc5
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2024-01-03 13:37:04 +0100

    feat(z13): updated flake and enabled printing

--- hosts/nabokikh-z13/configuration.nix
+++ hosts/nabokikh-z13/configuration.nix
@@ -94,7 +94,7 @@
   environment.localBinInPath = true;
 
   # Enable CUPS to print documents.
-  services.printing.enable = false;
+  services.printing.enable = true;
 
   # Enable sound with pipewire.
   sound.enable = true;
