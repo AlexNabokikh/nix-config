6186a01c94726c8fd8dcbd77735ab83263690226
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2024-02-10 11:16:57 +0100

    feat(zsh): moved from omz and p10k to starship

--- home/modules/zsh.nix
+++ home/modules/zsh.nix
@@ -3,38 +3,85 @@
   # TODO: replace exa alias with eza once auto-complete fixed
   programs.zsh = {
     enable = true;
-    enableCompletion = false;
+    enableCompletion = true;
     shellAliases = {
       cat = "bat -p";
+
+      # git
+      gaa = "git add --all";
+      gcam = "git commit --all --message";
+      gco = "git checkout";
+
       du = "dust";
       grep = "rg";
       kupdate = "krew update && krew upgrade";
+
+      # kubectl
+      k = "kubectl";
+      kgp = "kubectl get pods";
+      kep = "kubectl edit pods";
+      kdp = "kubectl describe pods";
+      kdelp = "kubectl delete pods";
+      kgs = "kubectl get svc";
+      kes = "kubectl edit svc";
+      kds = "kubectl describe svc";
+      kdels = "kubectl delete svc";
+      kgi = "kubectl get ingress";
+      kei = "kubectl edit ingress";
+      kdi = "kubectl describe ingress";
+      kdeli = "kubectl delete ingress";
+      kgns = "kubectl get namespaces";
+      kens = "kubectl edit namespace";
+      kdns = "kubectl describe namespace";
+      kdelns = "kubectl delete namespace";
+      kgd = "kubectl get deployment";
+      ked = "kubectl edit deployment";
+      kdd = "kubectl describe deployment";
+      kdeld = "kubectl delete deployment";
+
       ld = "lazydocker";
       lg = "lazygit";
+
       repo = "cd $HOME/Documents/repositories";
       temp = "cd $HOME/Downloads/temp";
+
       top = "btm";
+
       v = "nvim";
+      vi = "nvim";
       vim = "nvim";
+
       ls = "exa --icons"; # default view
       ll = "exa -bhl --group-directories-first --icons"; # long list
       la = "exa -abhl --group-directories-first --icons"; # all list
       lt = "exa --tree --level=2 --icons"; # tree
     };
-    oh-my-zsh = {
-      enable = true;
-      plugins = [
-        "git"
-        "kubectl"
-        "vi-mode"
-      ];
-    };
     initExtra = ''
-      source "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme"
-      for conf in "$HOME/.config/zsh/"*.zsh; do
-        source "$conf"
-      done
-      unset conf
+      # kubectl auto-complete
+      source <(kubectl completion zsh)
+
+      # bindings
+      bindkey -s ^f "cd-to-project\n"
     '';
+  };
+
+  programs.starship = let
+    flavour = "macchiato";
+  in {
+    enable = true;
+    enableZshIntegration = true;
+    settings =
+      {
+        palette = "catppuccin_${flavour}";
+      }
+      // builtins.fromTOML (builtins.readFile
+        (pkgs.fetchFromGitHub
+          {
+            owner = "catppuccin";
+            repo = "starship";
+            rev = "5629d2356f62a9f2f8efad3ff37476c19969bd4f";
+            sha256 = "sha256-nsRuxQFKbQkyEI4TXgvAjcroVdG+heKX5Pauq/4Ota0=";
+          }
+          + /palettes/${flavour}.toml));
   };
 }
