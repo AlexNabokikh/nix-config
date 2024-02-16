04229685faca590d2bb1a1ae4610dc98591cb3a3
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2024-02-16 16:16:39 +0100

    feat(hyprland): waybar

--- home/modules/waybar.nix
+++ home/modules/waybar.nix
@@ -1,6 +1,9 @@
 {...}: let
   waybar = ./../../files/configs/waybar;
 in {
+  # Install waybar via home-manager module
+  programs.waybar.enable = true;
+
   # Source waybar config from the home-manager store
   home.file = {
     ".config/waybar" = {
