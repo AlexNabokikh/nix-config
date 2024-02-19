75f3a472eb82e21811569a46c4dcdbc590013251
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2024-02-19 16:19:53 +0100

    feat(hyprland): added clipboard manager

--- home/modules/clipboard.nix
+++ home/modules/clipboard.nix
@@ -1,8 +1,7 @@
-{pkgs, ...}: {
+{...}: {
   # Install cliphist via home-manager module
   services.cliphist = {
     enable = true;
-    package = pkgs.cliphist;
     systemdTarget = "hyprland-session.target";
   };
 }
--- home/modules/wofi.nix
+++ home/modules/wofi.nix
@@ -3,12 +3,13 @@
   programs.wofi = {
     enable = true;
     settings = {
+      insensitive = true;
       normal_window = true;
-      prompt = "";
+      prompt = "Search...";
       width = "50%";
       height = "40%";
-      key_up = "Ctrl+k";
-      key_down = "Ctrl+j";
+      key_up = "Ctrl-k";
+      key_down = "Ctrl-j";
     };
   };
 }
