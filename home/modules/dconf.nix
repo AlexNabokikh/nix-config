2f3f7651742c891203ec703c584ad12da29dfae9
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2024-02-20 20:13:28 +0100

    feat(home): added dconf settings

--- home/modules/dconf.nix
+++ home/modules/dconf.nix
@@ -10,6 +10,9 @@
         default-folder-viewer = "list-view";
         search-view = "list-view";
       };
+      "org/gtk/gtk4/settings/file-chooser" = {
+        show-hidden = true;
+      };
       "org/gnome/Weather" = {
         locations = "[<(uint32 2, <('Warsaw', 'EPWA', true, [(0.91048009894147275, 0.36593737231924195)], [(0.91193453416703718, 0.36651914291880922)])>)>]";
       };
