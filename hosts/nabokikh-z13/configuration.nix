859fda75f9ac7ccdc29da6d3a918433ba571f14f
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2024-02-29 08:57:21 +0100

    feat(z13): added z13 tlp profile

--- hosts/nabokikh-z13/configuration.nix
+++ hosts/nabokikh-z13/configuration.nix
@@ -10,6 +10,29 @@
   # Set hostname
   networking.hostName = "nabokikh-z13";
 
+  # Set TLP settings
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "balanced";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+
+      PLATFORM_PROFILE_ON_AC = "performance";
+      PLATFORM_PROFILE_ON_BAT = "low-power";
+
+      DEVICES_TO_DISABLE_ON_BAT_NOT_IN_USE = ["bluetooth" "wifi"];
+      DEVICES_TO_ENABLE_ON_AC = ["bluetooth" "wifi"];
+
+      DISK_IOSCHED = ["none"];
+
+      START_CHARGE_THRESH_BAT0 = 30;
+      STOP_CHARGE_THRESH_BAT0 = 80;
+    };
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
