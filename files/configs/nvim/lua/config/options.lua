ff4154d80d3e1677ba08328014bc555da7e7dd59
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2024-03-27 11:35:42 +0100

    feat(nvim): disable lazygit configuration from nvim

--- files/configs/nvim/lua/config/options.lua
+++ files/configs/nvim/lua/config/options.lua
@@ -2,3 +2,4 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.winbar = "%=%m %f"
+vim.g.lazygit_config = false
