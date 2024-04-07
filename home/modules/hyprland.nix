d10a969df71d64af5fbb391bef40ba9f9453459a
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2024-04-07 19:53:26 +0200

    feat(hyprlock): added dp and edp specific settings

--- home/modules/hyprland.nix
+++ home/modules/hyprland.nix
@@ -51,8 +51,9 @@ in {
           disable_loading_bar = true
       }
 
+      # DP-1 Conifg
       input-field {
-          monitor =
+          monitor = DP-1
           size = 250, 60
           outline_thickness = 2
           dots_size = 0.2 # Scale of input-field height, 0.2 - 0.8
@@ -72,7 +73,7 @@ in {
 
       # Date
       label {
-        monitor =
+        monitor = DP-1
         text = cmd[update:1000] echo "<span>$(date '+%A, %d %B')</span>"
         color = rgba(255, 255, 255, 0.8)
         font_size = 15
@@ -84,7 +85,7 @@ in {
 
       # Time
       label {
-          monitor =
+          monitor = DP-1
           text = cmd[update:1000] echo "<span>$(date '+%H:%M')</span>"
           color = rgba(255, 255, 255, 0.8)
           font_size = 120
@@ -96,11 +97,68 @@ in {
 
       # Keyboard layout
       label {
+        monitor = DP-1
         text = cmd[update:200] hyprctl devices -j | jq -r '.keyboards[] | select(.name == "at-translated-set-2-keyboard") | .active_keymap'
         color = rgba(255, 255, 255, 0.9)
         font_size = 10
         font_family = JetBrains Mono Nerd Font Mono
         position = 0, -175
+        halign = center
+        valign = center
+      }
+
+      # eDP-1 Conifg
+      input-field {
+          monitor = eDP-1
+          size = 500, 120
+          outline_thickness = 2
+          dots_size = 0.2 # Scale of input-field height, 0.2 - 0.8
+          dots_spacing = 0.2 # Scale of dots' absolute size, 0.0 - 1.0
+          dots_center = true
+          outer_color = rgba(0, 0, 0, 0)
+          inner_color = rgba(0, 0, 0, 0.5)
+          font_color = rgb(200, 200, 200)
+          fade_on_empty = false
+          capslock_color = -1
+          placeholder_text = <i><span foreground="##e6e9ef">Password</span></i>
+          hide_input = false
+          position = 0, -120
+          halign = center
+          valign = center
+      }
+
+      # Date
+      label {
+        monitor = eDP-1
+        text = cmd[update:1000] echo "<span>$(date '+%A, %d %B')</span>"
+        color = rgba(255, 255, 255, 0.8)
+        font_size = 30
+        font_family = JetBrains Mono Nerd Font Mono ExtraBold
+        position = 0, -400
+        halign = center
+        valign = top
+      }
+
+      # Time
+      label {
+          monitor = eDP-1
+          text = cmd[update:1000] echo "<span>$(date '+%H:%M')</span>"
+          color = rgba(255, 255, 255, 0.8)
+          font_size = 240
+          font_family = JetBrains Mono Nerd Font Mono ExtraBold
+          position = 0, -400
+          halign = center
+          valign = top
+      }
+
+      # Keyboard layout
+      label {
+        monitor = eDP-1
+        text = cmd[update:200] hyprctl devices -j | jq -r '.keyboards[] | select(.name == "at-translated-set-2-keyboard") | .active_keymap'
+        color = rgba(255, 255, 255, 0.9)
+        font_size = 20
+        font_family = JetBrains Mono Nerd Font Mono
+        position = 0, -230
         halign = center
         valign = center
       }
