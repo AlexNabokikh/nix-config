af3f7fc0c77e085b6f8eec505bc1394571b7d49f
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2024-04-16 11:43:46 +0200

    feat(krew): updated module

--- home/modules/krew.nix
+++ home/modules/krew.nix
@@ -9,6 +9,9 @@
     "ns"
     "stern"
   ];
+
+  # Convert the list of plugins into a space-separated string
+  krewPkgStr = lib.concatStringsSep " " krewPkgs;
 in {
   # Ensure krew package installed
   home.packages = with pkgs; [
@@ -22,9 +25,12 @@ in {
 
   # Install krew plugins
   home.activation.krew = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    export PATH="$PATH:${pkgs.git}/bin/:/usr/bin/";
-    export PATH="$PATH:$HOME/.krew/bin";
-    ${pkgs.krew}/bin/krew install ${builtins.toString krewPkgs}
-    ${pkgs.krew}/bin/krew upgrade
+    export PATH="$HOME/.krew/bin:${pkgs.git}/bin:/usr/bin:$PATH";
+
+    if [ -z "$(${pkgs.krew}/bin/krew list)" ]; then
+      ${pkgs.krew}/bin/krew install ${krewPkgStr}
+    else
+      ${pkgs.krew}/bin/krew upgrade
+    fi
   '';
 }
