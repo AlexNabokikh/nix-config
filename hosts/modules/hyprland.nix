09138e501e6f12d475f0d45bff67c977cab5f028
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2024-05-09 13:56:13 +0200

    feat(hyprland): changed default portal to wlr for better screen sharing

--- hosts/modules/hyprland.nix
+++ hosts/modules/hyprland.nix
@@ -14,6 +14,11 @@
   # Enable Hyprland
   programs.hyprland = {
     enable = true;
+    portalPackage =
+      pkgs.xdg-desktop-portal-wlr
+      // {
+        override = args: pkgs.xdg-desktop-portal-wlr.override (builtins.removeAttrs args ["hyprland"]);
+      };
   };
 
   # Enable security services
