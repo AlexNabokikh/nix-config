616c4fc16e1047617ec1d82eea559b60688fa59f
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2024-05-16 10:49:01 +0200

    feat(z13): ditched swap device

--- hosts/nabokikh-z13/hardware-configuration.nix
+++ hosts/nabokikh-z13/hardware-configuration.nix
@@ -26,10 +26,6 @@
     fsType = "vfat";
   };
 
-  swapDevices = [
-    {device = "/dev/disk/by-label/swap";}
-  ];
-
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
