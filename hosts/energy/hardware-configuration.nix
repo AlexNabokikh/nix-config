8a140672486457da1eda6e55a1fee01b03d88109
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2024-06-27 11:20:34 +0200

    feat(kernel): removed kvm-amd as it is not needed anymore

--- hosts/energy/hardware-configuration.nix
+++ hosts/energy/hardware-configuration.nix
@@ -13,7 +13,7 @@
 
   boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod"];
   boot.initrd.kernelModules = [];
-  boot.kernelModules = ["kvm-amd"];
+  boot.kernelModules = [];
   boot.extraModulePackages = [];
 
   fileSystems."/" = {
--- hosts/nabokikh-z13/hardware-configuration.nix
+++ hosts/nabokikh-z13/hardware-configuration.nix
@@ -13,7 +13,7 @@
 
   boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "thunderbolt" "usb_storage" "sd_mod"];
   boot.initrd.kernelModules = [];
-  boot.kernelModules = ["kvm-amd"];
+  boot.kernelModules = [];
   boot.extraModulePackages = [];
 
   fileSystems."/" = {
