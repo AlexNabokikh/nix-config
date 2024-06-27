7fd03e428b9a1daf10195f6c9723a9c162598a43
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2024-06-27 12:33:36 +0200

    feat(energy): enabled obs virtual camera

--- hosts/energy/hardware-configuration.nix
+++ hosts/energy/hardware-configuration.nix
@@ -14,7 +14,9 @@
   boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod"];
   boot.initrd.kernelModules = [];
   boot.kernelModules = [];
-  boot.extraModulePackages = [];
+  boot.extraModulePackages = with config.boot.kernelPackages; [
+    v4l2loopback
+  ];
 
   fileSystems."/" = {
     device = "/dev/disk/by-label/NIXROOT";
