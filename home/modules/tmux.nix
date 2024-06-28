496856a7bd142322294f3e847269cc07142cd8a2
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2024-06-28 12:08:04 +0200

    fix(tmux): avoid settting flavour via extraconfig

--- home/modules/tmux.nix
+++ home/modules/tmux.nix
@@ -6,7 +6,6 @@
     catppuccin = {
       enable = true;
       extraConfig = ''
-        set -g @catppuccin_flavour 'macchiato'
         set -g @catppuccin_window_left_separator "█"
         set -g @catppuccin_window_right_separator "█ "
         set -g @catppuccin_window_middle_separator " █"
