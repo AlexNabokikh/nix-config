a760478301c186230aaa2cf0c6cb980102d44d7c
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2024-07-11 11:37:41 +0200

    feat(fzf): sort out fzf options

--- home/modules/fzf.nix
+++ home/modules/fzf.nix
@@ -6,18 +6,18 @@
   home.sessionVariables = {
     FZF_DEFAULT_COMMAND = "find .";
     FZF_DEFAULT_OPTS = "
-    --height=40%
-    --layout=reverse
-    --info=inline
-    --multi
-    --preview-window=:hidden
-    --preview '([[ -f {}  ]] && (bat --color=always --style=numbers,changes {} || cat {})) || ([[ -d {}  ]] && (tree -C {} | less)) || echo {} 2> /dev/null | head -200'
-    --color='hl:148,hl+:154,pointer:032,marker:010,bg+:237,gutter:008'
-    --prompt='~ ' --pointer='▶' --marker='✓'
     --bind '?:toggle-preview'
     --bind 'ctrl-a:select-all'
-    --bind 'ctrl-y:execute-silent(echo {+} | wl-copy)'
     --bind 'ctrl-e:execute(echo {+} | xargs -o nvim)'
+    --bind 'ctrl-y:execute-silent(echo {+} | wl-copy)'
+    --color='hl:148,hl+:154,pointer:032,marker:010,bg+:237,gutter:008'
+    --height=40%
+    --info=inline
+    --layout=reverse
+    --multi
+    --preview '([[ -f {}  ]] && (bat --color=always --style=numbers,changes {} || cat {})) || ([[ -d {}  ]] && (tree -C {} | less)) || echo {} 2> /dev/null | head -200'
+    --preview-window=:hidden
+    --prompt='~ ' --pointer='▶' --marker='✓'
     ";
   };
 }
