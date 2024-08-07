c7f8465b63ad0a183479a1dfb1ad5c4c62b98fa9
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2024-08-07 09:56:39 +0200

    feat(common): refactored channels management

--- home/nabokikh/energy.nix
+++ home/nabokikh/energy.nix
@@ -1,31 +1,10 @@
 {...}: {
   imports = [
-    ../modules/alacritty.nix
-    ../modules/atuin.nix
-    ../modules/bat.nix
-    ../modules/bottom.nix
+    ../modules/common.nix
     ../modules/corectrl.nix
     ../modules/easyeffects.nix
-    ../modules/fastfetch.nix
-    ../modules/fzf.nix
-    ../modules/git.nix
-    ../modules/gpg.nix
-    ../modules/gtk.nix
     ../modules/hyprland.nix
-    ../modules/krew.nix
-    ../modules/lazygit.nix
-    ../modules/neovim.nix
-    ../modules/saml2aws.nix
-    ../modules/scripts.nix
-    ../modules/spicetify.nix
-    ../modules/tmux.nix
-    ../modules/ulauncher.nix
-    ../modules/zoom.nix
-    ../modules/zsh.nix
   ];
-
-  # Nixpkgs configuration
-  nixpkgs.config.allowUnfree = true;
 
   home = {
     username = "nabokikh";
