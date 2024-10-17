dedffb84847bdd8fa895f268473e7ba8efd6acaa
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2024-10-17 15:47:16 +0200

    chore(mac): trying to bootstrap mac to the flake

--- flake.nix
+++ flake.nix
@@ -1,6 +1,5 @@
 {
-  description = "NixOS configs for my machines";
-
+  description = "NixOS and nix-darwin configs for my machines";
   inputs = {
     # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
@@ -23,6 +22,12 @@
       url = "github:Gerg-L/spicetify-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    # Nix Darwin (for MacOS machines)
+    darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
@@ -30,6 +35,7 @@
     nixpkgs,
     home-manager,
     catppuccin,
+    darwin,
     ...
   } @ inputs: let
     inherit (self) outputs;
@@ -42,6 +48,12 @@
         gitKey = "C5810093";
         name = "nabokikh";
       };
+      alexander.nabokikh = {
+        email = "alexander.nabokikh@olx.pl";
+        fullName = "Alexander Nabokikh";
+        gitKey = "C5810093";
+        name = "alexander.nabokikh";
+      };
     };
 
     # Function for NixOS system configuration
@@ -52,6 +64,19 @@
           userConfig = users.${username};
         };
         modules = [./hosts/${hostname}/configuration.nix];
+      };
+
+    # Function for nix-darwin system configuration
+    mkDarwinConfiguration = hostname: username:
+      darwin.lib.darwinSystem {
+        specialArgs = {
+          inherit inputs outputs hostname;
+          userConfig = users.${username};
+        };
+        modules = [
+          ./hosts/${hostname}/configuration.nix
+          home-manager.darwinModules.home-manager
+        ];
       };
 
     # Function for Home Manager configuration
@@ -73,9 +98,14 @@
       nabokikh-z13 = mkNixosConfiguration "nabokikh-z13" "nabokikh";
     };
 
+    darwinConfigurations = {
+      "PL-OLX-H7236KQ94R" = mkDarwinConfiguration "PL-OLX-H7236KQ94R" "alexander.nabokikh";
+    };
+
     homeConfigurations = {
       "nabokikh@energy" = mkHomeConfiguration "x86_64-linux" "nabokikh" "energy";
       "nabokikh@nabokikh-z13" = mkHomeConfiguration "x86_64-linux" "nabokikh" "nabokikh-z13";
+      "alexander.nabokikh@PL-OLX-H7236KQ94R" = mkHomeConfiguration "aarch64-darwin" "alexander.nabokikh" "PL-OLX-H7236KQ94R";
     };
 
     overlays = import ./overlays {inherit inputs;};
---
+++ home/alexander.nabokikh/PL-OLX-H7236KQ94R.nix
@@ -0,0 +1,15 @@
+{...}: {
+  imports = [
+    ../modules/git.nix
+    ../modules/zsh.nix
+  ];
+
+  # Enable home-manager
+  programs.home-manager.enable = true;
+
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "24.05";
+}
