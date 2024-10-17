159aec862e05b4a78f4dec34fb9343b61243d804
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2024-10-17 15:55:24 +0200

    fix(mac): added state version for darwin

--- hosts/PL-OLX-H7236KQ94R/configuration.nix
+++ hosts/PL-OLX-H7236KQ94R/configuration.nix
@@ -6,5 +6,5 @@
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
   # this value at the release version of the first install of this system.
-  system.stateVersion = "24.05";
+  system.stateVersion = "5";
 }
