8382d59078f52e144532234da21daf9676dbd150
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2024-10-17 20:18:40 +0200

    fix(mac): wip

--- home/alexander.nabokikh/PL-OLX-H7236KQ94R.nix
+++ home/alexander.nabokikh/PL-OLX-H7236KQ94R.nix
@@ -1,20 +1,19 @@
 {...}: {
   imports = [
-    ../modules/git.nix
-    ../modules/zsh.nix
+    ../modules/go.nix
   ];
 
   # Enable home-manager
   programs.home-manager.enable = true;
 
-  home.username = "alexander.nabokikh";
-  home.homeDirectory = "/Users/alexander.nabokikh";
+  # home.username = "alexander.nabokikh";
+  # home.homeDirectory = "/Users/alexander.nabokikh";
 
   # Catpuccin flavor and accent
-  catppuccin = {
-    flavor = "macchiato";
-    accent = "lavender";
-  };
+  # catppuccin = {
+  #   flavor = "macchiato";
+  #   accent = "lavender";
+  # };
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
