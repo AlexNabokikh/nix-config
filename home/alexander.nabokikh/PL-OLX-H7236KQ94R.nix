73f9b83eeac20917c2ac966829d93afa73c783a0
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2024-10-17 20:31:30 +0200

    fix(mac): wip

--- home/alexander.nabokikh/PL-OLX-H7236KQ94R.nix
+++ home/alexander.nabokikh/PL-OLX-H7236KQ94R.nix
@@ -1,6 +1,23 @@
 {...}: {
   imports = [
+    ../modules/alacritty.nix
+    ../modules/atuin.nix
+    ../modules/bat.nix
+    ../modules/bottom.nix
+    ../modules/fastfetch.nix
+    ../modules/fzf.nix
+    ../modules/git.nix
     ../modules/go.nix
+    ../modules/gpg.nix
+    ../modules/gtk.nix
+    ../modules/krew.nix
+    ../modules/lazygit.nix
+    ../modules/neovim.nix
+    ../modules/saml2aws.nix
+    ../modules/spicetify.nix
+    ../modules/tmux.nix
+    ../modules/zoom.nix
+    ../modules/zsh.nix
   ];
 
   # Enable home-manager
