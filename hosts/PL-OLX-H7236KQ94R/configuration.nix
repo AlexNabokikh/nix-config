deb82bba134c3fac9403afdd7fae1c909f20d606
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2024-10-19 21:12:14 +0200

    feat(mac): added keyboard remappings

--- hosts/PL-OLX-H7236KQ94R/configuration.nix
+++ hosts/PL-OLX-H7236KQ94R/configuration.nix
@@ -36,59 +36,65 @@
   security.pam.enableSudoTouchIdAuth = true;
 
   # System settings
-  system.defaults = {
-    NSGlobalDomain = {
-      AppleInterfaceStyle = "Dark";
-      ApplePressAndHoldEnabled = false;
-      AppleShowAllExtensions = true;
-      KeyRepeat = 2;
-      NSAutomaticCapitalizationEnabled = false;
-      NSAutomaticDashSubstitutionEnabled = false;
-      NSAutomaticQuoteSubstitutionEnabled = false;
-      NSAutomaticSpellingCorrectionEnabled = false;
-      NSAutomaticWindowAnimationsEnabled = false;
-      NSDocumentSaveNewDocumentsToCloud = false;
-      NSNavPanelExpandedStateForSaveMode = true;
-      PMPrintingExpandedStateForPrint = true;
+  system = {
+    defaults = {
+      NSGlobalDomain = {
+        AppleInterfaceStyle = "Dark";
+        ApplePressAndHoldEnabled = false;
+        AppleShowAllExtensions = true;
+        KeyRepeat = 2;
+        NSAutomaticCapitalizationEnabled = false;
+        NSAutomaticDashSubstitutionEnabled = false;
+        NSAutomaticQuoteSubstitutionEnabled = false;
+        NSAutomaticSpellingCorrectionEnabled = false;
+        NSAutomaticWindowAnimationsEnabled = false;
+        NSDocumentSaveNewDocumentsToCloud = false;
+        NSNavPanelExpandedStateForSaveMode = true;
+        PMPrintingExpandedStateForPrint = true;
+      };
+      LaunchServices = {
+        LSQuarantine = false;
+      };
+      trackpad = {
+        TrackpadRightClick = true;
+        TrackpadThreeFingerDrag = true;
+        Clicking = true;
+      };
+      finder = {
+        AppleShowAllFiles = true;
+        FXDefaultSearchScope = "SCcf";
+        CreateDesktop = false;
+        FXEnableExtensionChangeWarning = false;
+        QuitMenuItem = true;
+        ShowPathbar = true;
+        ShowStatusBar = true;
+        _FXShowPosixPathInTitle = true;
+        _FXSortFoldersFirst = true;
+      };
+      dock = {
+        expose-animation-duration = 0.15;
+        show-recents = false;
+        showhidden = true;
+        persistent-apps = [
+          "/Applications/Brave Browser.app"
+          "${pkgs.alacritty}/Applications/Alacritty.app"
+          "${pkgs.telegram-desktop}/Applications/Telegram.app"
+        ];
+        tilesize = 30;
+        wvous-bl-corner = 1;
+        wvous-br-corner = 1;
+        wvous-tl-corner = 1;
+        wvous-tr-corner = 1;
+      };
+      screencapture = {
+        location = "/Users/${userConfig.name}/Downloads/temp";
+        type = "png";
+        disable-shadow = true;
+      };
     };
-    LaunchServices = {
-      LSQuarantine = false;
-    };
-    trackpad = {
-      TrackpadRightClick = true;
-      TrackpadThreeFingerDrag = true;
-      Clicking = true;
-    };
-    finder = {
-      AppleShowAllFiles = true;
-      FXDefaultSearchScope = "SCcf";
-      CreateDesktop = false;
-      FXEnableExtensionChangeWarning = false;
-      QuitMenuItem = true;
-      ShowPathbar = true;
-      ShowStatusBar = true;
-      _FXShowPosixPathInTitle = true;
-      _FXSortFoldersFirst = true;
-    };
-    dock = {
-      expose-animation-duration = 0.15;
-      show-recents = false;
-      showhidden = true;
-      persistent-apps = [
-        "/Applications/Brave Browser.app"
-        "${pkgs.alacritty}/Applications/Alacritty.app"
-        "${pkgs.telegram-desktop}/Applications/Telegram.app"
-      ];
-      tilesize = 30;
-      wvous-bl-corner = 1;
-      wvous-br-corner = 1;
-      wvous-tl-corner = 1;
-      wvous-tr-corner = 1;
-    };
-    screencapture = {
-      location = "/Users/${userConfig.name}/Downloads/temp";
-      type = "png";
-      disable-shadow = true;
+    keyboard = {
+      swapLeftCtrlAndFn = true;
+      nonUS.remapTilde = true;
     };
   };
 
