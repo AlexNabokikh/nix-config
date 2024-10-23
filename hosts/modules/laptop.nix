2c43468366464c1b414268a69ed51878c9eaf031
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2024-10-23 13:04:33 +0200

    feat(tlp): prioritized silent operation and long battery life for laptop

--- hosts/modules/laptop.nix
+++ hosts/modules/laptop.nix
@@ -4,17 +4,18 @@
     tlp = {
       enable = true;
       settings = {
-        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_AC = "schedutil";
         CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
-        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "balance-power";
         CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
 
-        PLATFORM_PROFILE_ON_AC = "performance";
+        PLATFORM_PROFILE_ON_AC = "low-power";
         PLATFORM_PROFILE_ON_BAT = "low-power";
 
         USB_EXCLUDE_BTUSB = 1;
 
+        RADEON_DPM_PERF_LEVEL_ON_AC = "auto";
         RADEON_DPM_PERF_LEVEL_ON_BAT = "low";
 
         DISK_IOSCHED = ["none"];
