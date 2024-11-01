d9cbf57af6c1accfd0277e2265600b6b7a94f35e
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2024-11-01 18:13:05 +0100

    feat(tmux): adjusted config for a new version

--- home/modules/tmux.nix
+++ home/modules/tmux.nix
@@ -6,18 +6,13 @@
     catppuccin = {
       enable = true;
       extraConfig = ''
-        set -g @catppuccin_window_left_separator "█"
-        set -g @catppuccin_window_right_separator "█ "
-        set -g @catppuccin_window_middle_separator " █"
-        set -g @catppuccin_window_number_position "right"
-        set -g @catppuccin_window_default_fill "number"
-        set -g @catppuccin_window_default_text "#W"
-        set -g @catppuccin_window_current_fill "number"
-        set -g @catppuccin_window_current_text "#W"
-        set -g @catppuccin_status_modules_right "host date_time"
-        set -g @catppuccin_status_left_separator "█"
-        set -g @catppuccin_status_right_separator "█"
-        set -g @catppuccin_date_time_text "%Y-%m-%d %H:%M"
+        set -g @catppuccin_flavor "macchiato"
+        set -g @catppuccin_status_background "none"
+        set -g @catppuccin_window_text " #W"
+        set -g @catppuccin_window_current_text " #W"
+        set -g @catppuccin_window_current_number_color "#{@thm_peach}"
+        set -g status-left ""
+        set -g status-right ""
       '';
     };
     escapeTime = 10;
