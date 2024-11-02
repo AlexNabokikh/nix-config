61b14aa18f8c6f3fdf5267a46ffe8566d29d5ec0
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2024-11-02 10:17:39 +0100

    feat(tmux): return old tmux look

--- home/modules/tmux.nix
+++ home/modules/tmux.nix
@@ -8,11 +8,17 @@
       extraConfig = ''
         set -g @catppuccin_flavor "macchiato"
         set -g @catppuccin_status_background "none"
-        set -g @catppuccin_window_text " #W"
-        set -g @catppuccin_window_current_text " #W"
+
         set -g @catppuccin_window_current_number_color "#{@thm_peach}"
+        set -g @catppuccin_window_current_text " #W"
+        set -g @catppuccin_window_current_text_color "#{@thm_bg}"
+        set -g @catppuccin_window_number_color "#{@thm_blue}"
+        set -g @catppuccin_window_text " #W"
+        set -g @catppuccin_window_text_color "#{@thm_surface_0}"
+        set -g @catppuccin_status_left_separator "█"
+
+        set -g status-right "#{E:@catppuccin_status_host}#{E:@catppuccin_status_date_time}"
         set -g status-left ""
-        set -g status-right ""
       '';
     };
     escapeTime = 10;
