f2d72b0a52c913babab2daf15c1f43a731189718
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2024-12-11 14:00:14 +0100

    chore(neovim): removed unused keymap

--- files/configs/nvim/lua/config/keymaps.lua
+++ files/configs/nvim/lua/config/keymaps.lua
@@ -2,7 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<leader>aa", "<cmd>Dashboard<CR>", { desc = "Go to Dashboard Menu" })
 vim.keymap.set("n", "<leader>fa", function()
   return require("telescope.builtin").find_files({
     find_command = { "rg", "--files", "--hidden", "-g", "!.git/" },
