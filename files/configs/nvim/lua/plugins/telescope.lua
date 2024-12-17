77be7afa36a4679705a439e413c7273fedb61c42
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2024-12-17 11:56:14 +0100

    chore(nvim): adjusted nvim config to the lazyvim changes

--- files/configs/nvim/lua/plugins/telescope.lua
+++ files/configs/nvim/lua/plugins/telescope.lua
@@ -1,12 +1,5 @@
 return {
   "telescope.nvim",
-  dependencies = {
-    "nvim-telescope/telescope-fzf-native.nvim",
-    build = "make",
-    config = function()
-      require("telescope").load_extension("fzf")
-    end,
-  },
   opts = {
     defaults = {
       layout_strategy = "horizontal",
