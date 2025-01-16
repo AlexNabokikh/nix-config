9579b1803b713751e26a9234fdc4d087f632a3f5
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2025-01-16 16:36:53 +0100

    feat(home): converted fastfetch to the home-manager module

--- home/modules/fastfetch.nix
+++ home/modules/fastfetch.nix
@@ -1,16 +1,112 @@
-{pkgs, ...}: let
-  fastfetch_config = ./../../files/configs/fastfetch;
-in {
-  # Install fastfetch via home-manager package
-  home.packages = with pkgs; [
-    fastfetch
-  ];
-
-  # Source fastfetch config from the home-manager store
-  xdg.configFile = {
-    "fastfetch" = {
-      recursive = true;
-      source = "${fastfetch_config}";
+{...}: {
+  # Install and configure fastfetch via home-manager module
+  programs.fastfetch = {
+    enable = true;
+    settings = {
+      logo = {
+        type = "none";
+      };
+      display = {
+        separator = "->   ";
+      };
+      modules = [
+        {
+          type = "title";
+          format = "{6}{7}{8}";
+        }
+        "break"
+        {
+          type = "custom";
+          format = "┌───────────────────────────── System Information ─────────────────────────────┐";
+        }
+        "break"
+        {
+          key = "     OS           ";
+          keyColor = "red";
+          type = "os";
+        }
+        {
+          key = "    󰌢 Machine      ";
+          keyColor = "green";
+          type = "host";
+        }
+        {
+          key = "     Kernel       ";
+          keyColor = "magenta";
+          type = "kernel";
+        }
+        {
+          key = "    󰏖 Packages     ";
+          type = "packages";
+        }
+        {
+          key = "    󰅐 Uptime       ";
+          keyColor = "red";
+          type = "uptime";
+        }
+        {
+          key = "    󰍹 Resolution   ";
+          keyColor = "yellow";
+          type = "display";
+          compactType = "original-with-refresh-rate";
+        }
+        {
+          key = "     WM           ";
+          keyColor = "blue";
+          type = "wm";
+        }
+        {
+          key = "     DE           ";
+          keyColor = "green";
+          type = "de";
+        }
+        {
+          key = "     Shell        ";
+          keyColor = "cyan";
+          type = "shell";
+        }
+        {
+          key = "     Terminal     ";
+          keyColor = "red";
+          type = "terminal";
+        }
+        {
+          key = "    󰻠 CPU          ";
+          keyColor = "yellow";
+          type = "cpu";
+        }
+        {
+          key = "    󰍛 GPU          ";
+          keyColor = "blue";
+          type = "gpu";
+        }
+        {
+          key = "    󰑭 Memory       ";
+          keyColor = "magenta";
+          type = "memory";
+        }
+        {
+          key = "    󰩟 Local IP     ";
+          keyColor = "red";
+          type = "localip";
+        }
+        {
+          key = "    󰩠 Public IP    ";
+          keyColor = "cyan";
+          type = "publicip";
+        }
+        "break"
+        {
+          type = "custom";
+          format = "└──────────────────────────────────────────────────────────────────────────────┘";
+        }
+        "break"
+        {
+          paddingLeft = 34;
+          symbol = "circle";
+          type = "colors";
+        }
+      ];
     };
   };
 }
