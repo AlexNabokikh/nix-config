5d2398dbc5abb9a699dc525428f9007dd1b0df4f
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2025-03-18 14:47:07 +0100

    feat(k9s): added aliases

--- modules/home-manager/programs/k9s/default.nix
+++ modules/home-manager/programs/k9s/default.nix
@@ -1,6 +1,41 @@
 {...}: {
   # Install k9s via home-manager module
-  programs.k9s.enable = true;
+  programs.k9s = {
+    enable = true;
+    settings.k9s = {
+      ui = {
+        headless = true;
+        logoless = true;
+      };
+    };
+    hotkey.hotKeys = {
+      shift-1 = {
+        shortCut = "Shift-1";
+        description = "Show pods";
+        command = "pods";
+      };
+      shift-2 = {
+        shortCut = "Shift-2";
+        description = "Show deployments";
+        command = "dp";
+      };
+      shift-3 = {
+        shortCut = "Shift-3";
+        description = "Show nodes";
+        command = "nodes";
+      };
+      shift-4 = {
+        shortCut = "Shift-4";
+        description = "Show services";
+        command = "services";
+      };
+      shift-5 = {
+        shortCut = "Shift-5";
+        description = "Show Ingress";
+        command = "ingress";
+      };
+    };
+  };
 
   # Enable catppuccin theming for k9s
   catppuccin.k9s.enable = true;
