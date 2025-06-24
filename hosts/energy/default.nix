10fd01163581a1ed2f2738ba0cf116b7c444a88c
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2025-06-24 16:15:14 +0200

    feat(energy): go back to hyprland

--- hosts/energy/default.nix
+++ hosts/energy/default.nix
@@ -11,7 +11,7 @@
 
     ./hardware-configuration.nix
     "${nixosModules}/common"
-    "${nixosModules}/desktop/kde"
+    "${nixosModules}/desktop/hyprland"
     "${nixosModules}/programs/steam"
   ];
 
