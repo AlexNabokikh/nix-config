e2aa359773a737d2ff591b4954449499bb32de4d
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2025-06-28 11:58:45 +0200

    feat(host/common): moved to the kernel 6.15

--- modules/nixos/common/default.nix
+++ modules/nixos/common/default.nix
@@ -38,7 +38,7 @@
 
   # Boot settings
   boot = {
-    kernelPackages = pkgs.linuxKernel.packages.linux_6_14;
+    kernelPackages = pkgs.linuxKernel.packages.linux_6_15;
     consoleLogLevel = 0;
     initrd.verbose = false;
     kernelParams = ["quiet" "splash" "rd.udev.log_level=3"];
