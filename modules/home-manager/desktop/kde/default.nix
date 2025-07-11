217f2edf67475494be0971a77525a660e60a99b5
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2025-07-11 13:30:01 +0200

    feat(kde): disable DoNotDisturb when screen sharing

--- modules/home-manager/desktop/kde/default.nix
+++ modules/home-manager/desktop/kde/default.nix
@@ -495,6 +495,7 @@
           FocusPolicy = "FocusFollowsMouse";
         };
       };
+      plasmanotifyrc.DoNotDisturb.WhenScreenSharing = false;
       plasmarc.OSD.Enabled = false;
       spectaclerc = {
         Annotations.annotationToolType = 8;
