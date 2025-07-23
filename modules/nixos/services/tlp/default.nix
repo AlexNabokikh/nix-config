7e7602e97e1800f3474fd3a6e7d1813a61fddb84
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2025-07-23 14:07:21 +0200

    feat(tlp): finetuned tlp for z13

--- modules/nixos/services/tlp/default.nix
+++ modules/nixos/services/tlp/default.nix
@@ -6,19 +6,38 @@
       settings = {
         CPU_SCALING_GOVERNOR_ON_AC = "schedutil";
         CPU_SCALING_GOVERNOR_ON_BAT = "schedutil";
+        CPU_BOOST_ON_AC = 1;
+        CPU_BOOST_ON_BAT = 0;
+        CPU_SCALING_MIN_FREQ_ON_BAT = 400000;
+        CPU_SCALING_MAX_FREQ_ON_BAT = 3000000;
 
         CPU_ENERGY_PERF_POLICY_ON_AC = "balance_power";
-        CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
 
         PLATFORM_PROFILE_ON_AC = "low-power";
         PLATFORM_PROFILE_ON_BAT = "low-power";
 
         USB_EXCLUDE_BTUSB = 1;
+        USB_AUTOSUSPEND = 1;
+        USB_AUTOSUSPEND_DISABLE_ON_SHUTDOWN = 1;
 
-        RADEON_DPM_PERF_LEVEL_ON_AC = "auto";
-        RADEON_DPM_PERF_LEVEL_ON_BAT = "auto";
+        AMDGPU_ABM_LEVEL_ON_AC = 0;
+        AMDGPU_ABM_LEVEL_ON_BAT = 3;
 
         DISK_IOSCHED = ["none"];
+        DISK_APM_LEVEL_ON_BAT = "1 1";
+
+        SATA_LINKPWR_ON_BAT = "min_power";
+        PCIE_ASPM_ON_AC = "performance";
+        PCIE_ASPM_ON_BAT = "powersupersave";
+
+        RUNTIME_PM_ON_AC = "on";
+        RUNTIME_PM_ON_BAT = "auto";
+
+        WIFI_PWR_ON_BAT = "on";
+
+        SOUND_POWER_SAVE_ON_BAT = 1;
+        SOUND_POWER_SAVE_CONTROLLER = "Y";
 
         # Battery charge thresholds for on-road usage
         START_CHARGE_THRESH_BAT0 = 85;
@@ -32,4 +51,7 @@
 
   # Disable fingerprint reader
   services.fprintd.enable = false;
+
+  # For thermal management (AMD-specific)
+  services.thermald.enable = true;
 }
