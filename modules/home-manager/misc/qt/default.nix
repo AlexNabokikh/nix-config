717d1e31aaeda4d616d8936d23b54492321093de
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2025-08-10 18:42:13 +0300

    chore(qt): removed redundant env vars

--- modules/home-manager/misc/qt/default.nix
+++ modules/home-manager/misc/qt/default.nix
@@ -17,12 +17,6 @@
     home.sessionVariables = {
       # use wayland as the default backend, fallback to xcb if wayland is not available
       QT_QPA_PLATFORM = "wayland;xcb";
-
-      # remain backwards compatible with qt5
-      DISABLE_QT5_COMPAT = "0";
-
-      # tell calibre to use the dark theme
-      CALIBRE_USE_DARK_PALETTE = "1";
     };
   };
 }
