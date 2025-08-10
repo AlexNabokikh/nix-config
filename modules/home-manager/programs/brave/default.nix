a938dfd478064c3f7ca3dcb206b3b969a4822f1b
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2025-08-10 18:57:08 +0300

    chore(xdg): refactored brave module

--- modules/home-manager/programs/brave/default.nix
+++ modules/home-manager/programs/brave/default.nix
@@ -1,24 +1,14 @@
 { pkgs, lib, ... }:
 {
   # Ensure Brave browser package installed
-  home.packages = with pkgs; [ brave ];
+  home.packages = [ pkgs.brave ];
 
   xdg = lib.mkIf (!pkgs.stdenv.isDarwin) {
-    mimeApps.defaultApplications = {
-      "application/x-extension-htm" = [ "brave-browser.desktop" ];
-      "application/x-extension-html" = [ "brave-browser.desktop" ];
-      "application/x-extension-shtml" = [ "brave-browser.desktop" ];
-      "application/x-extension-xht" = [ "brave-browser.desktop" ];
-      "application/x-extension-xhtml" = [ "brave-browser.desktop" ];
-      "application/xhtml+xml" = [ "brave-browser.desktop" ];
-      "text/html" = [ "brave-browser.desktop" ];
-      "x-scheme-handler/about" = [ "brave-browser.desktop" ];
-      "x-scheme-handler/chrome" = [ "brave-browser.desktop" ];
-      "x-scheme-handler/ftp" = [ "brave-browser.desktop" ];
-      "x-scheme-handler/http" = [ "brave-browser.desktop" ];
-      "x-scheme-handler/https" = [ "brave-browser.desktop" ];
-      "x-scheme-handler/unknown" = [ "brave-browser.desktop" ];
-      "application/pdf" = [ "brave-browser.desktop" ];
+    enable = true;
+
+    mimeApps = {
+      enable = true;
+      defaultApplications = lib.xdg.mimeAssociations [ pkgs.brave ];
     };
   };
 }
