09968f8c1cb8253270da9e683aea603b026a6006
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2025-08-10 18:43:14 +0300

    chore(brave): minor style change

--- modules/home-manager/programs/brave/default.nix
+++ modules/home-manager/programs/brave/default.nix
@@ -1,33 +1,24 @@
-{
-  pkgs,
-  lib,
-  ...
-}:
+{ pkgs, lib, ... }:
 {
   # Ensure Brave browser package installed
-  home.packages = with pkgs; [
-    brave
-  ];
+  home.packages = with pkgs; [ brave ];
 
-  # Apply XDG configuration only on non-Darwin platforms
   xdg = lib.mkIf (!pkgs.stdenv.isDarwin) {
-    mimeApps = {
-      defaultApplications = {
-        "application/x-extension-htm" = "brave-browser.desktop";
-        "application/x-extension-html" = "brave-browser.desktop";
-        "application/x-extension-shtml" = "brave-browser.desktop";
-        "application/x-extension-xht" = "brave-browser.desktop";
-        "application/x-extension-xhtml" = "brave-browser.desktop";
-        "application/xhtml+xml" = "brave-browser.desktop";
-        "text/html" = "brave-browser.desktop";
-        "x-scheme-handler/about" = "brave-browser.desktop";
-        "x-scheme-handler/chrome" = [ "chromium-browser.desktop" ];
-        "x-scheme-handler/ftp" = "brave-browser.desktop";
-        "x-scheme-handler/http" = "brave-browser.desktop";
-        "x-scheme-handler/https" = "brave-browser.desktop";
-        "x-scheme-handler/unknown" = "brave-browser.desktop";
-        "application/pdf" = "brave-browser.desktop";
-      };
+    mimeApps.defaultApplications = {
+      "application/x-extension-htm" = [ "brave-browser.desktop" ];
+      "application/x-extension-html" = [ "brave-browser.desktop" ];
+      "application/x-extension-shtml" = [ "brave-browser.desktop" ];
+      "application/x-extension-xht" = [ "brave-browser.desktop" ];
+      "application/x-extension-xhtml" = [ "brave-browser.desktop" ];
+      "application/xhtml+xml" = [ "brave-browser.desktop" ];
+      "text/html" = [ "brave-browser.desktop" ];
+      "x-scheme-handler/about" = [ "brave-browser.desktop" ];
+      "x-scheme-handler/chrome" = [ "brave-browser.desktop" ];
+      "x-scheme-handler/ftp" = [ "brave-browser.desktop" ];
+      "x-scheme-handler/http" = [ "brave-browser.desktop" ];
+      "x-scheme-handler/https" = [ "brave-browser.desktop" ];
+      "x-scheme-handler/unknown" = [ "brave-browser.desktop" ];
+      "application/pdf" = [ "brave-browser.desktop" ];
     };
   };
 }
