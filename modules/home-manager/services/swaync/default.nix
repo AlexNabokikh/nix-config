e1c1ffe216320c71a3a293bb75aed9aa50d6ae0f
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2025-08-13 21:57:55 +0300

    feat(swaync): revamp and support swaync 0.12+

--- modules/home-manager/services/swaync/default.nix
+++ modules/home-manager/services/swaync/default.nix
@@ -3,10 +3,18 @@
   # Install swaync via home-manager module
 
   # temp switch to stable swaync: https://github.com/catppuccin/swaync/issues/20
-  home.packages = [ pkgs.stable.swaynotificationcenter ];
+  home.packages = [ pkgs.swaynotificationcenter ];
 
   # Source swaync config from the home-manager store
   xdg.configFile = {
+    "swaync/config.json".text = ''
+      {
+        "notification-body-image-height": 100,
+        "notification-body-image-width": 200,
+        "notification-icon-size": 32
+      }
+    '';
+
     "swaync/style.css" = {
       source = ./style.css;
     };
