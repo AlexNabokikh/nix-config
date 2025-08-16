0303251129b11ee8c0f4fd0603a303aa88b28d90
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2025-08-16 13:29:23 +0300

    feat(neovim): show hidden files by default

---
+++ modules/home-manager/programs/neovim/lazyvim/lua/plugins/snacks.lua
@@ -0,0 +1,21 @@
+return {
+  "snacks.nvim",
+  opts = {
+    picker = {
+      sources = {
+        files = {
+          hidden = true,
+          ignored = true,
+        },
+        grep = {
+          hidden = true,
+          ignored = true,
+        },
+        explorer = {
+          hidden = true,
+          ignored = true,
+        },
+      },
+    },
+  },
+}
