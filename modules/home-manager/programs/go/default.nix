146a69ab2c7235b9e1e293255ee753d6f116166f
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2025-09-19 12:22:31 +0200

    feat(go): adjusted config to the latest home-manager module changes

--- modules/home-manager/programs/go/default.nix
+++ modules/home-manager/programs/go/default.nix
@@ -1,12 +1,13 @@
-{ ... }:
+{ config, ... }:
+let
+  GOPATH = "${config.home.homeDirectory}/go";
+  GOBIN = "${GOPATH}/bin";
+in
 {
   # Install and configure Golang via home-manager module
   programs.go = {
     enable = true;
-    env = {
-      GOBIN = "go/bin";
-      GOPATH = "go";
-    };
+    env = { inherit GOBIN GOPATH; };
   };
 
   # Ensure Go bin in the PATH
