450212fb1353ea546599f74be77d7ff9163b4ade
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2025-11-13 09:46:30 +0100

    feat(swaync): removed temp overlay

--- modules/home-manager/services/swaync/default.nix
+++ modules/home-manager/services/swaync/default.nix
@@ -1,27 +1,5 @@
 { ... }:
 {
-  # Tempoary use overlay until new release
-  nixpkgs.overlays = [
-    (self: prev: {
-      swaynotificationcenter = prev.swaynotificationcenter.overrideAttrs (oldAttrs: {
-        src = prev.fetchFromGitHub {
-          owner = "ErikReider";
-          repo = "SwayNotificationCenter";
-          rev = "d95a18896b0013cdc5193c63ceb34fef1e956f02";
-          sha256 = "sha256-+9d/qhsw4GOgdraNj0i94n8BY5LF+NV48+YpyijukDk=";
-        };
-        version = "0.12.2-pr654";
-        nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ prev.blueprint-compiler ];
-        buildInputs = oldAttrs.buildInputs ++ [
-          prev.gtk4
-          prev.gtk4-layer-shell
-          prev.libadwaita
-          prev.pantheon.granite7
-        ];
-      });
-    })
-  ];
-
   # Manage swaync service via Home-manager
   services.swaync = {
     enable = true;
