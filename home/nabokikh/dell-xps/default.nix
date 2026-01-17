c78311a50bd4ac75fcfcbf57d68938c42690e7bb
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2026-01-17 18:14:02 +0100

    feat: added test laptop

---
+++ home/nabokikh/dell-xps/default.nix
@@ -0,0 +1,13 @@
+{ nhModules, ... }:
+{
+  imports = [
+    "${nhModules}/common"
+    "${nhModules}/desktop/hyprland"
+  ];
+
+  # Enable home-manager
+  programs.home-manager.enable = true;
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "26.05";
+}
