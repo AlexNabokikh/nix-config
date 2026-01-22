7f30c4a6dfed9e0d755156ff2453c3d81e2a1112
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2026-01-22 11:33:47 +0100

    feat(xdg): hide unused desktop entries

--- modules/home-manager/misc/xdg/default.nix
+++ modules/home-manager/misc/xdg/default.nix
@@ -6,6 +6,22 @@
   xdg = {
     enable = true;
 
+    # Hide the following desktop entries for launcher.
+    desktopEntries = {
+      uuctl = {
+        name = "uuctl";
+        noDisplay = true;
+      };
+      qt6ct = {
+        name = "qt6ct";
+        noDisplay = true;
+      };
+      kvantummanager = {
+        name = "kvantum";
+        noDisplay = true;
+      };
+    };
+
     mimeApps = {
       enable = true;
       defaultApplicationPackages = [
