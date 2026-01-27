de3f3a73330a475fdefb4e2bd760844516d76fc5
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2026-01-27 11:20:00 +0100

    fix(hyprland): added temp fix for gdm+hyprland-uwsm issue

--- modules/nixos/desktop/hyprland/default.nix
+++ modules/nixos/desktop/hyprland/default.nix
@@ -11,6 +11,20 @@
     xwayland.enable = true;
   };
 
+  # FIXME: https://github.com/NixOS/nixpkgs/issues/484328
+  services.displayManager.defaultSession = "hyprland-uwsm";
+
+  programs.uwsm = {
+    enable = true;
+    waylandCompositors = {
+      hyprland = {
+        prettyName = "Hyprland";
+        comment = "Hyprland compositor managed by UWSM";
+        binPath = "/run/current-system/sw/bin/start-hyprland";
+      };
+    };
+  };
+
   # Hyprland specific packages
   environment.systemPackages = with pkgs; [
     grimblast
