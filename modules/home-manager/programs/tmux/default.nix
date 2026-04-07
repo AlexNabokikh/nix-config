06063af615abbe7a8bcc739d76e46b0a7ef82e84
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2026-04-07 07:49:38 +0200

    chore: cleanup tmux config

--- modules/home-manager/programs/tmux/default.nix
+++ modules/home-manager/programs/tmux/default.nix
@@ -63,17 +63,15 @@
       };
 
       catppuccin.tmux.extraConfig = ''
-        set -g @catppuccin_flavor "${config.catppuccin.flavor}"
         set -g @catppuccin_status_background "none"
-
+        set -g @catppuccin_window_status_style "basic"
         set -g @catppuccin_window_current_number_color "#{@thm_peach}"
         set -g @catppuccin_window_current_text " #W"
         set -g @catppuccin_window_current_text_color "#{@thm_bg}"
         set -g @catppuccin_window_number_color "#{@thm_blue}"
         set -g @catppuccin_window_text " #W"
-        set -g @catppuccin_window_text_color "#{@thm_surface_0}"
-        set -g @catppuccin_window_status_left_separator "█"
-
+        set -g @catppuccin_status_left_separator "█"
+        set -g @catppuccin_status_right_separator "█"
         set -g status-right "#{E:@catppuccin_status_host}#{E:@catppuccin_status_date_time}"
         set -g status-left ""
       '';
