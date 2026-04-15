7f71f6718e2ca5ca094a24348f57760884380bfe
Fabrice Semti <fabrice@fabricesemti.com>
2026-04-15 09:10:06 +0100

    refactor(docker): remove deprecated service configurations and related files

--- hosts/trinity/configuration.nix
+++ hosts/trinity/configuration.nix
@@ -1,30 +1,5 @@
 {...}: {
   imports = [
     ../vm-generic/configuration.nix
-    ../modules/docker
   ];
-
-  services.arcane = {
-    enable = true;
-  };
-
-  services.cloudflared-tunnel = {
-    enable = true;
-  };
-
-  services.traefik-docker = {
-    enable = true;
-  };
-
-  services.whoami-docker = {
-    enable = true;
-  };
-
-  services.beszel = {
-    enable = true;
-  };
-
-  services.omni-docker = {
-    enable = true;
-  };
 }
