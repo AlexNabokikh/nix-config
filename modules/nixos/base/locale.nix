7cb919fde9e32839a93fca76537f1d4432085094
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2026-04-18 17:33:32 +0200

    chore(nixos/base): better formatting

--- modules/nixos/base/locale.nix
+++ modules/nixos/base/locale.nix
@@ -1,9 +1,11 @@
 { ... }:
 {
-  flake.modules.nixos.base = { config, ... }: {
-    time.timeZone = config.profile.locale.timezone;
+  flake.modules.nixos.base =
+    { config, ... }:
+    {
+      time.timeZone = config.profile.locale.timezone;
 
-    i18n.defaultLocale = config.profile.locale.default;
-    i18n.extraLocaleSettings = config.profile.locale.extra;
-  };
+      i18n.defaultLocale = config.profile.locale.default;
+      i18n.extraLocaleSettings = config.profile.locale.extra;
+    };
 }
