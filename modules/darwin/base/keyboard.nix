f7637e2e718e70f7f6ce9baa049265e1517592c1
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2026-05-14 12:37:17 +0200

    feat(darwin): simplified tilde remap

--- modules/darwin/base/keyboard.nix
+++ modules/darwin/base/keyboard.nix
@@ -2,12 +2,7 @@
   flake.modules.darwin.base = {
     system.keyboard = {
       enableKeyMapping = true;
-      userKeyMapping = [
-        {
-          HIDKeyboardModifierMappingDst = 30064771125;
-          HIDKeyboardModifierMappingSrc = 30064771172;
-        }
-      ];
+      nonUS.remapTilde = true;
     };
   };
 }
