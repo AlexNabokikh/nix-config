c5b35bb6e7effadf52636150ab36b75f97c757bd
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2026-05-19 19:46:48 +0200

    chore: refactored noctalia module

--- modules/nix-settings.nix
+++ modules/nix-settings.nix
@@ -20,10 +20,6 @@
             "nix-command"
             "flakes"
           ];
-          extra-substituters = [ "https://noctalia.cachix.org" ];
-          extra-trusted-public-keys = [
-            "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
-          ];
         };
 
         optimise.automatic = true;
