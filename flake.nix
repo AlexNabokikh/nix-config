5631578cad9db9d57f3ea80ee4bee7d2c7e00cd8
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2026-06-01 15:15:36 +0200

    chore: bumped flake and addressed eval warnings

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
--- modules/desktop/compositor-common.nix
+++ modules/desktop/compositor-common.nix
@@ -3,19 +3,44 @@ let
   hm = config.flake.modules.homeManager;
 in
 {
-  flake.modules.nixos.compositorCommon = {
-    services = {
-      displayManager.gdm.enable = true;
-      power-profiles-daemon.enable = true;
-      upower.enable = true;
-      gnome.gnome-keyring.enable = true;
-    };
+  flake.modules.nixos.compositorCommon =
+    { config, pkgs, ... }:
+    {
+      services = {
+        displayManager.gdm.enable = true;
+        power-profiles-daemon.enable = true;
+        upower.enable = true;
+        gnome.gnome-keyring.enable = true;
+      };
 
-    security = {
-      polkit.enable = true;
-      pam.services.gdm.enableGnomeKeyring = true;
+      environment.sessionVariables.XDG_DATA_DIRS = [ "${pkgs.gdm}/share" ];
+
+      security = {
+        polkit.enable = true;
+        pam.services = {
+          # FIXME: Temporary workaround for nixpkgs#523332
+          gdm.enableGnomeKeyring = true;
+          gdm-launch-environment.rules.session.gnome-session-path = {
+            order = config.security.pam.services.gdm-launch-environment.rules.session.systemd.order + 50;
+            control = "required";
+            modulePath = "${config.security.pam.package}/lib/security/pam_env.so";
+
+            settings = {
+              conffile =
+                let
+                  env = config.services.displayManager.generic.environment;
+                in
+                pkgs.writeText "gdm-launch-environment-env-conf" ''
+                  PATH                    DEFAULT="''${PATH}:${pkgs.gnome-session}/bin"
+                  XDG_DATA_DIRS           DEFAULT="''${XDG_DATA_DIRS}:${env.XDG_DATA_DIRS}"
+                  GDM_X_SERVER_EXTRA_ARGS DEFAULT="${env.GDM_X_SERVER_EXTRA_ARGS}"
+                '';
+              readenv = 0;
+            };
+          };
+        };
+      };
     };
-  };
 
   flake.modules.homeManager.compositorCommon =
     { pkgs, ... }:
