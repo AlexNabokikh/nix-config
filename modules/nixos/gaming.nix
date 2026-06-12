e1f85b29621915a288fcfb8cfbab3b3c749ef0bb
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2026-06-12 11:05:18 +0200

    feat(gaming): switched to proton ge

--- modules/nixos/gaming.nix
+++ modules/nixos/gaming.nix
@@ -1,20 +1,23 @@
 {
-  flake.modules.nixos.gaming = {
-    programs.steam = {
-      enable = true;
-      remotePlay.openFirewall = true;
-    };
+  flake.modules.nixos.gaming =
+    { pkgs, ... }:
+    {
+      programs.steam = {
+        enable = true;
+        remotePlay.openFirewall = true;
+        extraCompatPackages = [ pkgs.proton-ge-bin ];
+      };
 
-    boot.kernelParams = [
-      "split_lock_detect=off"
-      "vsyscall=emulate"
-    ];
+      boot.kernelParams = [
+        "split_lock_detect=off"
+        "vsyscall=emulate"
+      ];
 
-    services.pipewire.extraConfig.pipewire."10-gaming" = {
-      "context.properties" = {
-        "default.clock.quantum" = 256;
-        "default.clock.min-quantum" = 256;
+      services.pipewire.extraConfig.pipewire."10-gaming" = {
+        "context.properties" = {
+          "default.clock.quantum" = 256;
+          "default.clock.min-quantum" = 256;
+        };
       };
     };
-  };
 }
