4e25cacad636ef5c256badef9d97710a5064d8eb
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2026-06-15 14:43:18 +0200

    chore(xps): updated hardware configuration

--- modules/hosts/xps/_hardware.nix
+++ modules/hosts/xps/_hardware.nix
@@ -21,18 +21,22 @@
   ];
 
   fileSystems."/" = {
-    device = "/dev/disk/by-label/NIXROOT";
+    device = "/dev/disk/by-label/root";
     fsType = "ext4";
   };
 
   fileSystems."/boot" = {
-    device = "/dev/disk/by-label/NIXBOOT";
+    device = "/dev/disk/by-label/boot";
     fsType = "vfat";
     options = [
       "fmask=0077"
       "dmask=0077"
     ];
   };
+
+  swapDevices = [
+    { device = "/dev/disk/by-label/swap"; }
+  ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
