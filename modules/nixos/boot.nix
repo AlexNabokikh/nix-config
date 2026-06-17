c70c5a764ff26d7601a1e71889b7f29eb33ac5ef
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2026-06-17 20:26:59 +0200

    feat(base): switched to the latest kernel

--- modules/nixos/boot.nix
+++ modules/nixos/boot.nix
@@ -1,19 +1,22 @@
 {
-  flake.modules.nixos.boot = {
-    boot = {
-      consoleLogLevel = 0;
-      initrd.verbose = false;
-      kernelParams = [
-        "quiet"
-        "splash"
-        "rd.udev.log_level=3"
-      ];
-      loader = {
-        efi.canTouchEfiVariables = true;
-        systemd-boot.enable = true;
-        timeout = 0;
+  flake.modules.nixos.boot =
+    { pkgs, ... }:
+    {
+      boot = {
+        consoleLogLevel = 0;
+        initrd.verbose = false;
+        kernelPackages = pkgs.linuxPackages_latest;
+        kernelParams = [
+          "quiet"
+          "splash"
+          "rd.udev.log_level=3"
+        ];
+        loader = {
+          efi.canTouchEfiVariables = true;
+          systemd-boot.enable = true;
+          timeout = 0;
+        };
+        plymouth.enable = true;
       };
-      plymouth.enable = true;
     };
-  };
 }
