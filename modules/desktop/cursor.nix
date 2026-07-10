4c33a8e35cd478bf5dc9335694fb895b6287c06c
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2026-07-10 11:39:44 +0200

    chore(flake): bump deps

--- modules/desktop/cursor.nix
+++ modules/desktop/cursor.nix
@@ -2,7 +2,10 @@
   flake.modules.homeManager.cursor =
     { config, ... }:
     {
+      catppuccin.cursors.enable = false;
+
       home.pointerCursor = {
+        enable = true;
         gtk.enable = true;
         x11.enable = true;
         inherit (config.profile.appearance.cursorTheme) package name size;
