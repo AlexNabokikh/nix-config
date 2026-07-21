b04bb7fe3b7d9bd3ba9f501e0ca0f70468f5358a
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2026-07-21 17:05:31 +0200

    chore: removed default values

--- modules/programs/fzf.nix
+++ modules/programs/fzf.nix
@@ -41,13 +41,11 @@
         defaultCommand = "${pkgs.fd}/bin/fd --type f --hidden --follow --exclude .git";
         defaultOptions = [
           "--bind '?:toggle-preview'"
-          "--bind 'ctrl-a:select-all'"
           "--bind 'ctrl-e:execute(nvim -- {+})'"
           "--bind 'ctrl-y:execute-silent(echo {+} | ${copyCmd})'"
           "--height=40%"
           "--info=inline"
           "--layout=reverse"
-          "--multi"
           "--preview '( [[ -f {} ]] && (bat --color=always --style=numbers,changes {} || cat {}) || [[ -d {} ]] && eza --all --long --color=always {} || echo {} ) 2> /dev/null | head -200'"
           "--preview-window=:hidden"
           "--prompt='~ ' --pointer='▶' --marker='✓'"
