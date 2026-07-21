257c7a36f8421f08d1e5964033eb705a005d8517
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2026-07-21 17:06:49 +0200

    feat(nocatlia): reun apps as systemd services

--- modules/programs/noctalia.nix
+++ modules/programs/noctalia.nix
@@ -82,6 +82,7 @@
         settings = {
           shell = {
             font_family = uiFont;
+            launch_apps_as_systemd_services = true;
             polkit_agent = true;
             show_location = false;
 
