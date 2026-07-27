d6a512edcab2db0fa281da5b14c00135c734d542
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2026-07-27 21:16:03 +0200

    chore(eza): removed default option

--- modules/programs/eza.nix
+++ modules/programs/eza.nix
@@ -2,7 +2,6 @@
   flake.modules.homeManager.eza = {
     programs.eza = {
       enable = true;
-      enableZshIntegration = true;
       extraOptions = [ "--group-directories-first" ];
     };
   };
