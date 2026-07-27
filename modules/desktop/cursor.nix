07e876e84122c8cb5c5010ba56de28b6ca0bab3b
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2026-07-27 20:36:35 +0200

    chore(cursor): removed unused x11 option

--- modules/desktop/cursor.nix
+++ modules/desktop/cursor.nix
@@ -5,7 +5,6 @@
       home.pointerCursor = {
         enable = true;
         gtk.enable = true;
-        x11.enable = true;
         inherit (config.profile.appearance.cursorTheme) package name size;
       };
     };
