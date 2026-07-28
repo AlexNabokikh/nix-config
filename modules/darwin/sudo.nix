3cddb4c0563e8815d507b13bc8f891e87f89a65f
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2026-07-28 11:30:54 +0200

    chore(darwin): removed dead pam config

--- modules/darwin/sudo.nix
+++ modules/darwin/sudo.nix
@@ -2,7 +2,6 @@
   flake.modules.darwin.sudo =
     { config, ... }:
     {
-      security.pam.services.sudo_local.touchIdAuth = true;
       security.sudo.extraConfig = "${config.primaryUser}    ALL = (ALL) NOPASSWD: ALL";
     };
 }
