ad3694c028133e0eed2683db8af07c9bc54baea5
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2026-07-29 09:13:56 +0200

    feat(zsh): enabled vi mode

--- modules/programs/zsh.nix
+++ modules/programs/zsh.nix
@@ -17,19 +17,8 @@
 
       programs.zsh = {
         enable = true;
-        initContent = ''
-          # bindings
-          bindkey -e
-          bindkey '^H' backward-delete-word
-          bindkey '^[[1;5C' forward-word
-          bindkey '^[[1;5D' backward-word
-
-          # open commands in $EDITOR with C-v
-          autoload -z edit-command-line
-          zle -N edit-command-line
-          bindkey "^v" edit-command-line
-        ''
-        + lib.optionalString pkgs.stdenv.hostPlatform.isLinux ''
+        defaultKeymap = "viins";
+        initContent = lib.optionalString pkgs.stdenv.hostPlatform.isLinux ''
           open() {
             xdg-open "$@" </dev/null >/dev/null 2>&1 &!
           }
