d8fb3a862f86fa3d5b5ebb5ed2d73dbd26814ae3
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2026-08-07 13:55:49 +0200

    feat(gaming): move off proton-ge

--- modules/nixos/gaming.nix
+++ modules/nixos/gaming.nix
@@ -1,22 +1,17 @@
 {
-  flake.modules.nixos.gaming =
-    { pkgs, ... }:
-    {
-      programs.steam = {
-        enable = true;
-        extraCompatPackages = [ pkgs.proton-ge-bin ];
-      };
+  flake.modules.nixos.gaming = {
+    programs.steam.enable = true;
 
-      boot.kernelParams = [
-        "split_lock_detect=off"
-        "vsyscall=emulate"
-      ];
+    boot.kernelParams = [
+      "split_lock_detect=off"
+      "vsyscall=emulate"
+    ];
 
-      services.pipewire.extraConfig.pipewire."10-gaming" = {
-        "context.properties" = {
-          "default.clock.quantum" = 256;
-          "default.clock.min-quantum" = 256;
-        };
+    services.pipewire.extraConfig.pipewire."10-gaming" = {
+      "context.properties" = {
+        "default.clock.quantum" = 256;
+        "default.clock.min-quantum" = 256;
       };
     };
+  };
 }
