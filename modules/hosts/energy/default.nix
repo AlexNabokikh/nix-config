6d96b6c6c4061f13ebdb8f2a4f1b974491e70f09
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2026-09-23 15:25:21 +0200

    chore(hosts): removed default imports

--- modules/hosts/energy/default.nix
+++ modules/hosts/energy/default.nix
@@ -5,8 +5,7 @@ in
 {
   configurations.nixos.energy.module = {
     imports = [
-      inputs.nixos-hardware.nixosModules.common-cpu-amd-pstate
-      inputs.nixos-hardware.nixosModules.common-pc-ssd
+      inputs.nixos-hardware.nixosModules.common-cpu-amd
       inputs.nixos-hardware.nixosModules.common-gpu-amd
       ./_hardware.nix
       nixos.base
