ddbbbb79a1c2281cc794259ebc9cfa54c2c4d3b6
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2026-09-23 15:25:31 +0200

    chore(dconf): removed default settings

--- modules/desktop/desktop-apps.nix
+++ modules/desktop/desktop-apps.nix
@@ -17,25 +17,7 @@
         showtime
       ];
 
-      dconf.settings = {
-        "org/gnome/calculator" = {
-          accuracy = 9;
-          angle-units = "degrees";
-          base = 10;
-          button-mode = "basic";
-          number-format = "automatic";
-          show-thousands = false;
-          show-zeroes = false;
-          source-units = [ "degree" ];
-          target-units = [ "radian" ];
-          window-maximized = false;
-        };
-
-        "org/gnome/nautilus/preferences" = {
-          default-folder-viewer = "list-view";
-          search-filter-time-type = "last_modified";
-        };
-      };
+      dconf.settings."org/gnome/nautilus/preferences".default-folder-viewer = "list-view";
 
       xdg.mimeApps = {
         enable = true;
